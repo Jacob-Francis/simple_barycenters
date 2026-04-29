00bf808c45c481af5fa2cdff16dc8d86ec78fcb9
jacob <jjf817@ic.ac.uk>
2026-04-29 17:41:00 +0100

    s

--- plotting_grp10.py
+++ plotting_grp10.py
@@ -334,7 +334,7 @@ for data_set in [21, 22]:
         # top right full se
         ax_ss[1].plot(se_spread, obs_se,  marker=markers[aprox_type], color=aprox_colors[aprox_type], linestyle=line_style[data_set], markersize=12, markerfacecolor='none')
 
-        for i, (xi, yi) in enumerate(zip(x, y)):
+        for i, (xi, yi) in enumerate(zip(se_spread, obs_se)):
             if i in [0, len(x)-1]:  # annotate only the first and last points
                 ax_ss[1].annotate(
                     str(i),
